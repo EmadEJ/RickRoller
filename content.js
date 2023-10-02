5dfb92c383130a8f5c48fbede7df10548a3d11e7
EmadEJ <s.emad.emamjomeh@gmail.com>
2023-10-02 13:56:38 +0330

    fixed video link

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ console.log("YOOOOOOOOOOOOOOOOOO!");
 elements = document.getElementsByTagName("a");
 for (var i = 0; i < elements.length; i++) {
     if(Math.random() * 3 < 1) {
-        elements[i].href = "https://r.mtdv.me/cdn/rick.mp4"
+        elements[i].href = "https://cdn.mtdv.me/video/rick.mp4"
         console.log("Rickified: ", elements[i].innerHTML);
     }
 }
